4089a86869c5cf4095eda38d889d7cb210bc0163
MihlaliKota <hlalikota@gmail.com>
2024-03-31 21:55:06 +0200

    Updated carousel js code

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -15,34 +15,28 @@ const carouselTrack = document.querySelector('.carousel-track');
 const slides = document.querySelectorAll('.carousel-slide');
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
-
 let currentIndex = 0;
+const slideWidth = slides[0].offsetWidth + 10;
+
+const duplicateSlides = Array.from(slides).map(slide => slide.cloneNode(true));
+carouselTrack.append(...duplicateSlides);
+const totalSlides = slides.length * 2;
 
 function showSlide(index) {
-  const slideWidth = slides[0].offsetWidth + 10; // Add margin to calculate width
-  const maxIndex = slides.length - 1;
-
-  if (index < 0) {
-    index = maxIndex; // Loop to the last slide if reaching the beginning
-  } else if (index > maxIndex) {
-    index = 0; // Loop to the first slide if reaching the end
-  }
-
   carouselTrack.style.transform = `translateX(-${index * slideWidth}px)`;
   currentIndex = index;
 }
 
 prevBtn.addEventListener('click', () => {
-  showSlide(currentIndex - 1);
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  showSlide(currentIndex);
 });
 
 nextBtn.addEventListener('click', () => {
-  showSlide(currentIndex + 1);
+  currentIndex = (currentIndex + 1) % totalSlides;
+  showSlide(currentIndex);
 });
 
-setInterval(() => {
-  showSlide(currentIndex + 1);
-}, 3000);
+setInterval(() => { showSlide(currentIndex + 1); }, 3000);
 
-// Show the first slide initially
 showSlide(currentIndex);
