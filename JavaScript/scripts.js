3579765743f3d7abf8d538f751c762613fde323c
mambush <siphelo.Nyandani@capaciti.org.za>
2024-04-04 11:08:09 +0200

    updated carousel

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,42 +1,37 @@
-const buttons = document.querySelectorAll('.animated-button');
 
-buttons.forEach(button => {
-  button.addEventListener('mouseover', () => {
-    button.style.transform = 'scale(1.1)';
-  });
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImage = document.querySelectorAll('.carousel-slide img');
+// buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+//counter
+let counter = 1;
+const size = carouselImage[0].clientWidth;
 
-  button.addEventListener('mouseout', () => {
-    button.style.transform = 'scale(1)';
-  });
-});
-
-const carouselContainer = document.querySelector('.carousel-container');
-const carouselTrack = document.querySelector('.carousel-track');
-const slides = document.querySelectorAll('.carousel-slide');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-let currentIndex = 0;
-const slideWidth = slides[0].offsetWidth + 10;
-
-const duplicateSlides = Array.from(slides).map(slide => slide.cloneNode(true));
-carouselTrack.append(...duplicateSlides);
-const totalSlides = slides.length * 2;
-
-function showSlide(index) {
-  carouselTrack.style.transform = `translateX(-${index * slideWidth}px)`;
-  currentIndex = index;
-}
-
-prevBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-  showSlide(currentIndex);
-});
-
+carouselSlide.style.transform = 'translate X(' + (-size * counter) + 'px)';
+// Button listeners
 nextBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % totalSlides;
-  showSlide(currentIndex);
+  if (counter >= carouselImage.length - 1) return;
+  carouselSlide.style.transition = "transform 0,45 ease-in-out";
+  counter++;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px';
+});
+prevBtn.addEventListener('click', () => {
+  if (counter <= 0) return
+  carouselSlide.style.transition = "transform 0,45 ease-in-out";
+  counter--;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px';
+});
+carouselSlide.addEventListener('transitioned', () => {
+  if (carouselImage[counter].id === 'lastclone') {
+    carouselSlide.style.transition = "none";
+    counter = carouselImage.length - 2;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px';
+  }
+  if (carouselImage[counter].id === 'firstclone') {
+    carouselSlide.style.transition = "none";
+    counter = carouselImage.length - counter;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px';
+  }
 });
 
-setInterval(() => { showSlide(currentIndex + 1); }, 3000);
-
-showSlide(currentIndex);
